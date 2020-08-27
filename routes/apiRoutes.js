0ed9475904a3fb760e6309e8016c33c50a409084
Allie <williamson.alisonj@gmail.com>
2020-08-27 02:25:11 -0400

    api route that adds to json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
-const path = require("path");
-const filePath = path.join(__dirname, "../db/db.json");
-const data = require("../db/db.json");
+import { writeFile } from 'fs';
+import { join } from "path";
+const filePath = join(__dirname, "../db/db.json");
+import data, { push, indexOf, splice } from "../db/db.json";
 // used to give each note a unique id
-const shortid = require('shortid');
+import { generate } from 'shortid';
 
-module.exports = function(app) {
+export default function(app) {
 
 // displays notes
 app.get("/api/notes", function(req, res) {
@@ -17,14 +17,42 @@ app.post("/api/notes", function(req, res) {
 
     const newNote = req.body;
     // gives note specific id
-    newNote.id = shortid.generate();
+    newNote.id = generate();
     // pushes note data onto array
-    data.push(newNote);
-    fs.writeFile(filePath, JSON.stringify(data), err => {
+    push(newNote);
+    writeFile(filePath, JSON.stringify(data), err => {
         if (err) throw err;
         console.log("Note added");
         });
         // display in JSON
         res.send(newNote);
   });
-}
+
+// deletes a note
+app.delete("/api/notes/:id", function(req, res) {
+  const id = req.params.id;
+  for (let note of data) {
+      if (id === note.id) {
+        const noteIndex = indexOf(note);
+        splice(noteIndex, 1);
+        writeFile(filePath, JSON.stringify(data), err => {
+            if (err) throw err;
+            console.log('Note deleted');
+        });
+        res.end()
+      }
+    }
+});
+
+// displays a note
+app.get("/api/notes/:note", function(req, res) {
+    const chosen = req.params.note;
+    for (let i=0; i<readNotes.length; i++) {
+      if (chosen === readNotes[i].noteName) {
+        return res.json(readNotes[i]);
+      }
+    }
+    return res.json(false);
+  });
+} 
+ 
